ce0ae5f21028e42a8bffd9068a10cc3a89d3d150
Lars Halvor <larshalvor@vigilo.no>
2022-11-23 14:59:51 +0100

    Homefeed functionality

---
+++ js/homefeed.js
@@ -0,0 +1,108 @@
+"use strict";
+
+const homeFeed = document.querySelector("#home-feed");
+const API_BASE_URL = "https://nf-api.onrender.com";
+const loadMore = document.querySelector("#load-more");
+
+async function getWithToken(url) {
+  try {
+    // console.log(url);
+    const token = localStorage.getItem("token");
+    // console.log(token);
+    const fetchOptions = {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await fetch(url, fetchOptions);
+    // console.log("response token", response);
+    const json = await response.json();
+    console.log("json token", json);
+    // Loop through the data and create html //
+    json.forEach((post) => {
+      //   console.log(post);
+
+      const author = post.author.name;
+      const avatar = post.author.avatar;
+      const title = post.title;
+      const created = post.created.substring(0, 10);
+      const media = post.media;
+      const text = post.body;
+      const likes = post._count.reactions;
+      const comments = post._count.comments;
+
+      // remove loader //
+      // document.querySelector(".loader").style.display = "none";
+      //   /images/profileImages/profile01.jpg
+      // html //
+      homeFeed.innerHTML += `<div class="card mb-3 shadow">
+          <div
+            class="card-header bg-white d-flex justify-content-between border-bottom border-0"
+          >
+            <div class="d-flex">
+              <a href="profile.html" class="text-decoration-none text-dark"
+                ><img
+                  src="${avatar}" 
+                  alt="user avatar"
+                  class="post-avatar rounded-circle"
+                  onerror="this.onerror=null; this.src='https://img.freepik.com/free-vector/mysterious-mafia-man-wearing-hat_52683-34829.jpg?w=1380&t=st=1669211874~exp=1669212474~hmac=731dee4b6e9b61f93cf5e9547959b08ff3f5fb379e6996422a80d8e27ccaa2b4'"
+                />
+              </a>
+              <div class="d-flex flex-column ms-2 mt-1">
+                <h5 class="card-title mb-0">${author}</h5>
+                <p class="card-text">
+                  <small class="text-muted">Posted ${created}</small>
+                </p>
+              </div>
+            </div>
+            <!-- Dropdown start -->
+            <div class="dropdown">
+              <a
+                class=""
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <i class="bi bi-three-dots"></i>
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Hide post</a></li>
+              </ul>
+            </div>
+          </div>
+          <!-- Dropdown end -->     
+          <img class="card-img-top rounded-0" src="${media}" alt="Card image" onerror="this.style.display='none'"/>
+          <div class="card-body">
+          <h6 class="card-subtitle mb-2">${title}</h6>
+            <p class="card-text">
+              ${text}
+            </p>
+            <div class="d-flex">
+              <p class="bi bi-hand-thumbs-up me-4">&nbsp;${likes} Likes</p>
+              <p class="bi bi-chat-dots">&nbsp;${comments} Comments</p>
+            </div>
+            <div class="input-group">
+              <label for="text"></label>
+              <input
+                style="border-radius: 25px 0 0 25px"
+                type="text"
+                class="form-control bg-secondary bg-opacity-10"
+                placeholder="Add your comment.."
+              />
+              <button class="btn" style="border-radius: 0 25px 25px 0">
+                Post
+              </button>
+            </div>
+          </div>
+        </div>`;
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+const postsUrl = `${API_BASE_URL}/api/v1/social/posts/?_author=true&_comments=true&_reactions=true`;
+
+getWithToken(postsUrl);
--- js/login.js
+++ js/login.js
@@ -1,39 +1,39 @@
-'use strict';
+"use strict";
 
-const API_BASE_URL = 'https://nf-api.onrender.com';
-const errorMessage = document.querySelector('.error-message');
-const loginBtn = document.querySelector('#login-button');
+const API_BASE_URL = "https://nf-api.onrender.com";
+const errorMessage = document.querySelector(".error-message");
+const loginBtn = document.querySelector("#login-button");
 
 async function loginUser(url, userData) {
   try {
-    console.log('user data', url, userData);
+    console.log("user data", url, userData);
     const postData = {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(userData),
     };
     const response = await fetch(url, postData);
 
-    console.log('postdata ', postData);
-    console.log('response ', response);
+    console.log("postdata ", postData);
+    console.log("response ", response);
     const json = await response.json();
-    console.log('json ', json);
+    console.log("json ", json);
     const token = json.accessToken;
-    localStorage.setItem('token', token);
+    localStorage.setItem("token", token);
   } catch (error) {
-    console.log('error', error);
+    console.log("error", error);
   }
 }
 const loginUrl = `${API_BASE_URL}/api/v1/social/auth/login`;
 
 const userLogin = {
-  email: document.querySelector('#email').value,
-  password: document.querySelector('#password').value,
+  email: document.querySelector("#email").value,
+  password: document.querySelector("#password").value,
 };
 
-// loginBtn.onclick = loginUser(loginUrl, userLogin);
+loginBtn.onclick = loginUser(loginUrl, userLogin);
 
 //   name: john_the_blacksmith
 //   email: blacksmith_john@noroff.no
@@ -41,26 +41,65 @@ const userLogin = {
 
 // REFACTOR IN TO RE-USABLE CODE MAYBE
 
-async function getWithToken(url, options = {}) {
-  try {
-    console.log(url);
-    const token = localStorage.getItem('token');
-    console.log(token);
-    const fetchOptions = {
-      method: 'GET',
-      headers: {
-        'Content-type': 'application/json',
-        Authorization: `Bearer ${token}`,
-      },
-    };
-    const response = await fetch(url, fetchOptions);
-    console.log(response);
-    const json = await response.json();
-    console.log(json);
-  } catch (error) {
-    console.log(error);
-  }
+{
+  /* <div class="card mb-3 shadow">
+            <div
+              class="card-header bg-white d-flex justify-content-between border-0"
+            >
+              <div class="d-flex">
+                <a href="profile.html" class="text-decoration-none text-dark"
+                  ><img
+                    src="/images/profileImages/profile01.jpg"
+                    alt="user avatar"
+                    class="post-avatar rounded-circle"
+                  />
+                </a>
+                <div class="d-flex flex-column ms-2 mt-1">
+                  <h5 class="mb-0">John Denver Smithsonian</h5>
+                  <p class="card-text">
+                    <small class="text-muted">Posted 3 mins ago</small>
+                  </p>
+                </div>
+              </div>
+              <!-- Dropdown start -->
+              <div class="dropdown">
+                <a
+                  class=""
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <i class="bi bi-three-dots"></i>
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Hide post</a></li>
+                </ul>
+              </div>
+            </div>
+            <!-- Dropdown end -->
+            <div class="card-body">
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <div class="d-flex">
+                <p class="bi bi-hand-thumbs-up me-4">&nbsp;Like</p>
+                <p class="bi bi-chat-dots">&nbsp;Comment</p>
+              </div>
+              <div class="input-group">
+                <label for="text"></label>
+                <input
+                  style="border-radius: 25px 0 0 25px"
+                  type="text"
+                  class="form-control bg-secondary bg-opacity-10"
+                  placeholder="Add your comment.."
+                />
+                <button class="btn" style="border-radius: 0 25px 25px 0">
+                  Post
+                </button>
+              </div>
+            </div>
+          </div> */
 }
-const postsUrl = `${API_BASE_URL}/api/v1/social/posts`;
-
-getWithToken(postsUrl);
